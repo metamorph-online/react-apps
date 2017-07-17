70771fcaceb4e005dbb07219093dc8acb5977972
Oleg Lazarenko <metamorph_online@yahoo.com>
2017-07-17 18:51:51 +0300

    db changed, added description and cover images

--- blog-app/src/containers/post_data.js
+++ blog-app/src/containers/post_data.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {getBooksData} from '../actions/index';
+import renderHTML from 'react-render-html';
 
 class BooksIndex extends Component{
 
@@ -9,24 +10,30 @@ class BooksIndex extends Component{
 		this.props.getBooksData();
 	}
 
+	
 	renderBooks(){
 		return(
 
 		<div>
 			<h3>List of available books</h3>
 
-			<ul class="list-group">
+			<ul className="list-group">
 				{this.props.books.map((book)=>{
 
 					return (
-						<li key={book.id} className="list-group-item">							
-								<ul class="list-group">
-									<li className="list-group-item active"><h4>Name: {book.name} </h4></li>
-									<li className="list-group-item">Pages: {book.pages_i}</li>
-									<li className="list-group-item">Genre: {book.genre_s}</li>
-									<li className="list-group-item">Price: {book.price}</li>
-									<li className="list-group-item">Series: {book.series_t}</li>
-								</ul>
+						<li key={book.id} className="list-group-item">
+
+								<div className="cover-img"><img src={book.cover_img} alt="" className="img-responsive" /></div>
+
+								<div className="book-info">
+									<ul className="list-group">
+										<li className="list-group-item active"><h4>Name: {book.name} </h4></li>
+										<li className="list-group-item">Pages: {book.pages_i}</li>
+										<li className="list-group-item">Genre: {book.genre_s}</li>
+										<li className="list-group-item">Price: {book.price}</li>
+										<li className="list-group-item">Series: {renderHTML(book.description)}</li>
+									</ul>
+								</div>
 						</li>
 					);
 				})}
